d300547f52c71604d2469ca0e73754f6b28f85dd
Aleksa Yaroshenko <yaroshenkooi@ukma.edu.ua>
2018-10-10 16:52:23 +0300

    Add files via upload

---
+++ practice_4.3/practice4.3.js
@@ -0,0 +1,5 @@
+//Перепишите скрипт модуля числа из первого задания при помощи тернарного оператора. Пришлите результат в виде ссылки на github
+
+let num = prompt("Enter a number"); 
+
+let answer = (num > 0) ? console.log(num): console.log(-num);
